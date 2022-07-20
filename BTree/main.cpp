004aff008e163ed794375b00c728baf07f198bab
Angel Choquehuanca Anconeyra <84652181+AngelAnconeyra@users.noreply.github.com>
2022-07-19 22:00:57 -0500

    Update main.cpp

--- BTree/main.cpp
+++ BTree/main.cpp
@@ -9,20 +9,32 @@ int main(){
     ejem.insert(29);
     ejem.insert(17);
     ejem.insert(25);
+
     ejem.insert(21);
     ejem.insert(15);
     ejem.insert(31);
+
     ejem.insert(13);
+
     ejem.insert(51);
+
     ejem.insert(20);
     ejem.insert(24);
+
     ejem.insert(48);
     ejem.insert(19);
     ejem.insert(60);
+
     ejem.insert(35);
     ejem.insert(66);
-    ejem.insert(70);
+
+    //ejem.insert(70);
+
+    //ejem.insert(9);
+    //ejem.insert(11);
+    //ejem.insert(14);
     ejem.imprimir();
-    //cout<< ejem.getT();
+    //ejem.imprimirHojas();
+    //ejem.buscarRango(11,19);
     return 0;
 }
