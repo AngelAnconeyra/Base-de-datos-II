17f25706df4add72c89bd670f7e4fbe9459ea828
Angel Choquehuanca Anconeyra <84652181+AngelAnconeyra@users.noreply.github.com>
2022-07-19 22:00:40 -0500

    Update BTree.h

--- BTree/BTree.h
+++ BTree/BTree.h
@@ -18,8 +18,12 @@ class BTree{
 
         //Funciones
         void insert(int);
+        void buscarRango(int,int);
+        void buscarRango(Nodo *,int,int);
+        void erase(int);
         void imprimir();
         void imprimir(Nodo*,int);
+        void imprimirHojas();
         ~BTree();
 };
 
@@ -54,8 +58,53 @@ void BTree::insert(int data){
     }
 }
 
+void BTree::buscarRango(int L,int R){
+    Nodo *aux=root;
+    buscarRango(aux,L,R);
+}
+
+void BTree::buscarRango(Nodo* aux,int L,int R){
+    int i=t;
+    if(aux->getLeaf()){
+        int j=0;
+        while(aux->getKey()[j]<L && j<aux->getN()){
+            j++;
+        }
+        do{
+            for(int k=j;k<aux->getN();k++){
+                if(aux->getKey()[k]<=R){
+                    cout<< aux->getKey()[k] <<" ";
+                }
+                else{
+                    return;
+                }
+            }
+            j=0;
+            aux = aux->getChild(aux,t);
+        } while (aux !=nullptr);
+    }
+    else{
+        while(!aux->getKey()[i-1]){
+            i--;
+        }
+        //Si el valor es mayor que el que esta en la parte extema derecha, le inserta a la derecha
+        while(aux->getKey()[i-1]>L && i!=0){
+            i--;
+        }
+        buscarRango(aux->getChild(aux,i),L,R);
+    }
+}
+
+void BTree::erase(int data){
+    if(!this->root){
+        cout<<"\nBTree vacio\n";
+    }
+    else{
+        root->erase(root,data,t,root);
+    }
+}
+
 void BTree::imprimir(){
-    cout<<"pasa\n";
     if(!this->root){
         cout<<"\nVacio\n";
     }
@@ -76,6 +125,23 @@ void BTree::imprimir(Nodo *aux,int i){
             i++;
         }
     }
+}
+
+void BTree::imprimirHojas(){
+    Nodo *aux=root;
+    while (aux->getLeaf()!=true){
+        aux = aux->getChild(aux,0);
+    }
+    //cout<< "pasa\n";
+    do{
+        for(int i=0;i<aux->getN();i++){
+            cout<< aux->getKey()[i] <<" ";
+        }
+        cout<<endl;
+        aux = aux->getChild(aux,t);
+    } while (aux !=nullptr);
+
+    
 }
 
 BTree::~BTree(){
